1631056a6461453b7f51f6f592159a1106287ed6
aditya <aditya.goyal9219@gmail.com>
2017-09-09 15:31:40 +0530

    add README.MD

--- server.js
+++ server.js
@@ -1,93 +1,29 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
 
-// https://devcenter.heroku.com/articles/mongolab
-// http://todomvc.com/examples/angularjs/#/
-var express  = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
+app.use(bodyParser.urlencoded({extended: true}))
 
-    // Mongoose Schema definition
-    Schema = new mongoose.Schema({
-      id       : String, 
-      title    : String,
-      completed: Boolean
-    }),
+var MONGOLAB_URI = "mongodb://aditya9219:engineer001@ds131119.mlab.com:31119/db_aditya9219",db;
 
-    Todo = mongoose.model('Todo', Schema);
-	
-/*
- * I’m sharing my credential here.
- * Feel free to use it while you’re learning.
- * After that, create and use your own credential.
- * Thanks.
- *
- * MONGOLAB_URI=mongodb://example:example@ds053312.mongolab.com:53312/todolist
- * 'mongodb://example:example@ds053312.mongolab.com:53312/todolist'
- */
-mongoose.connect(process.env.MONGOLAB_URI, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
-
-express()
-  // https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
-  .use(bodyParser.json()) // support json encoded bodies
-  .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
-
-  .get('/api', function (req, res) {
-    res.json(200, {msg: 'OK' });
+MongoClient.connect(process.env.MONGOLAB_URI || MONGOLAB_URI, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
   })
+})
 
-  .get('/api/todos', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#query_Query-find
-    Todo.find( function ( err, todos ){
-      res.json(200, todos);
-    });
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.post('/mood', (req, res) => {
+  db.collection('mood').save(req.body, (err, result) => {
+   if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
   })
-
-  .post('/api/todos', function (req, res) {
-    var todo = new Todo( req.body );
-    todo.id = todo._id;
-    // http://mongoosejs.com/docs/api.html#model_Model-save
-    todo.save(function (err) {
-      res.json(200, todo);
-    });
-  })
-
-  .del('/api/todos', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#query_Query-remove
-    Todo.remove({ completed: true }, function ( err ) {
-      res.json(200, {msg: 'OK'});
-    });
-  })
-
-  .get('/api/todos/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
-    Todo.findById( req.params.id, function ( err, todo ) {
-      res.json(200, todo);
-    });
-  })
-
-  .put('/api/todos/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
-    Todo.findById( req.params.id, function ( err, todo ) {
-      todo.title = req.body.title;
-      todo.completed = req.body.completed;
-      // http://mongoosejs.com/docs/api.html#model_Model-save
-      todo.save( function ( err, todo ){
-        res.json(200, todo);
-      });
-    });
-  })
-
-  .del('/api/todos/:id', function (req, res) {
-    // http://mongoosejs.com/docs/api.html#model_Model.findById
-    Todo.findById( req.params.id, function ( err, todo ) {
-      // http://mongoosejs.com/docs/api.html#model_Model.remove
-      todo.remove( function ( err, todo ){
-        res.json(200, {msg: 'OK'});
-      });
-    });
-  })
-
-  .use(express.static(__dirname + '/'))
-  .listen(process.env.PORT || 5000);
+})
